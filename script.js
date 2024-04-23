68152b6229318fe745dea7887030aadf036ae1c3
Gerald Chacon <chacongerald.20@gmail.com>
2024-04-22 23:39:56 -0300

    2da Pre Entrega

--- script.js
+++ script.js
@@ -1,43 +1,5 @@
 principal()
 
-//Compra de Producto
-
-let valor = Number(prompt("Ingrese monto disponible"))
-if (valor > 0 && valor < 1000) {
-    if (valor >= 95) {
-        alert("Puedes comprar Pantalon, Camisa, Zapatillas y Medias")
-    } else if (valor >= 90) {
-        alert("Puedes comprar Pantalon, Camisa y Zapatillas")
-    } else if (valor >= 50) {
-        alert("Puedes comprar Pantalon y Camisa")
-    } else if (valor >= 40) {
-        alert("Puedes comprar Zapatillas")
-    } else if (valor >= 30) {
-        alert("Puedes comprar Pantalon")
-    } else if (valor >= 20) {
-        alert("Puedes comprar Camisa")
-    } else if (valor >= 5) {
-        alert("Puedes comprar Medias")
-    } else {
-        alert("No te alcanza el dinero")
-    }
-} else {
-    alert("Dato fuera del parametro")
-} 
-
-
-principal()
-
-//Costo total productos
-
-for (let i = 0; i < 5; i++) {
-    let producto = prompt("Ingrese Producto").toUpperCase()
-    let precio = prompt("Ingrese Precio")
-    alert(producto + " cuesta " + precio)
-}
-
-//Funcion
-
 function saludar(nombreCompleto) {
     alert("Bienvenido/a " + nombreCompleto + "!")
 }
@@ -47,6 +9,21 @@ function principal() {
     saludar(nombre)
 }
 
+let productos = [
+    { id: 1, nombre: "royal canin", categoria: "alimentos", stock: 20, precio: 70000,},
+    { id: 2, nombre: "proplan", categoria: "alimentos", stock: 30, precio: 10000,},
+    { id: 3, nombre: "bocaditos", categoria: "snack", stock: 20, precio: 4500,},
+    { id: 4, nombre: "dental care", categoria: "snack", stock: 14, precio: 4200,},
+    { id: 5, nombre: "shampoo", categoria: "higiene", stock: 10, precio: 4000,},
+]
+
+let valorIngresado = prompt("Ingrese categoria para filtrar").toLowerCase()
+let productosFiltrados = productos.filter(producto => producto.categoria.toLowerCase() === valorIngresado)
+alert("productos.categoria")
 
 
-
+// let productoBuscado = productos.find(function (producto) {return producto.id === 5})
+// let productoBuscado = productos.find(producto => producto.id === 5)
+// if (productoBuscado) {
+// alert("productoBuscado")
+// }
